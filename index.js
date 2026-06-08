d1f05fbf25b89ba568b200c23a09b6de9a214a0c
Boat <r.wongsaphan@gmail.com>
2026-06-08 10:29:56 +0700

    test push

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000')
+  console.log('Saaaerver is running on http://localhost:3000')
 })
